f6af21cc761ac052f4a97ff8c37d30bc5095fc48
Santiago Acosta Verrier <santiav@gmail.com>
2024-01-26 20:27:44 -0300

    query users DB

--- back/routes/routes.js
+++ back/routes/routes.js
@@ -1,10 +1,19 @@
 import { Router } from 'express';
+import pool from "../config/conn.js";
 const router = Router();
 
 
 // Login
-router.get("/", (req, res) => {
-      res.send("Login...")
+router.get("/", async (req, res) => {
+   try {
+
+      const [rows] = await pool.query("SELECT * FROM cuentas");
+      res.send(rows)
+
+   } catch (err) {
+      return res.status(500).json({ message: "Algo salió mal " + err });
+   }
+
 })
 
 export default router
