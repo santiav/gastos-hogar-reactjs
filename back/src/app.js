9e8e54f9c8e5789e91a42d4b22006dbfbbfdb19c
Santiago Acosta Verrier <santiav@gmail.com>
2024-02-29 15:09:08 -0300

    going back to just use cors()

--- back/src/app.js
+++ back/src/app.js
@@ -4,17 +4,10 @@ import routes from '../routes/routes.js'
 
 const app = express()
 
-// Configurar las opciones de CORS
-const corsOptions = {
-   origin: 'exquisite-banoffee-8c66ce.netlify.app/',
-   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-   allowedHeaders: ['Content-Type', 'Authorization'], 
-};
+
 
 // Middleware
-app.use(cors(corsOptions));
-// Habilitar CORS para todas las rutas
-app.options('*', cors(corsOptions))
+app.use(cors());
 // analizar JSON 
 app.use(express.json());
 // Analizar datos de formulario codificados en URL
